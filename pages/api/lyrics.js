9b58bdf5d36a6a9594909e7f66950bcd580f16ef
Thiago Ribeiro <thiago.ribeiro@0450-mbr.localdomain>
2020-04-05 19:54:14 +0200

    Project structure amendments

--- pages/api/lyrics.js
+++ pages/api/lyrics.js
@@ -1,3 +1,5 @@
+// zeit - https://zeit.co/docs/v2/serverless-functions/introduction
+
 export default (req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
